f24d2c1eaefb129c1cc384a27354fabdc1df7dca
Mwapsam <mwapsam@gmail.com>
2025-03-27 16:41:00 +0200

    Remove unused import from views.py to clean up code

--- trucker/views.py
+++ trucker/views.py
@@ -5,7 +5,6 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
-from rest_framework.response import JsonResponse
 
 from spotter.settings.serializers import CustomTokenObtainPairSerializer
 from trucker.exceptions import TripValidationError
